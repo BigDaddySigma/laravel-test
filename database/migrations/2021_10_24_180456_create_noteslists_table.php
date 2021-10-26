1b6735519c3c4e9839991a288fb12adf4851859e
BigDaddySigma <92638801+BigDaddySigma@users.noreply.github.com>
2021-10-26 22:21:33 +0200

    Add files via upload

---
+++ database/migrations/2021_10_24_180456_create_noteslists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoteslistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noteslists', function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->string('note_name');
+            $table->text('note_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noteslists');
+    }
+}
